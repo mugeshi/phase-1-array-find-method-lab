765290a4340320643c83e9de14c2b45e36538728
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-20 15:44:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+  const record = [
+    {year:"2015", result: "W"},
+    {year:"2014",  result:"N/A"},
+    {year: "2013",  result:"L"},
+  ]
+ 
+function superbowl(record)
+const winningRecord = record.find(item=>item.result=== "W");
+if (winningRecord) {
+  returnRecord.year;
+} else {
+    return undifined
+}
+
+ 
